a13bea8134a7fda55fa41cea8ed1a2f0380b33e5
AndreiMudrak <95141433+AndreiMudrak@users.noreply.github.com>
2022-05-26 11:40:02 +0300

    Update first.py

--- first.py
+++ first.py
@@ -1,6 +1,9 @@
 import mcpi.minecraft as minecraft
-craft = minecraft.Minecraft.create()
 
-cor = craft . player.getTilePos()
+mcraft = minecraft.Minecraft.create()
+myCoord = mcraft.player.getTilePos()
 
-print(cor.x, cor.y, cor.z)
+mcraft.postToChat ("Hello world!")
+mcraft.postToChat ("I'm here: x=" + str(myCoord.x) + " y=" + str(myCoord.y) + " z=" + str(myCoord.z))
+
+print ("My coordinate: x=" + str(myCoord.x) + " y=" + str(myCoord.y) + " z=" + str(myCoord.z))
