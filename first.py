9b9ff18b198b9ea75f188a258865e9536c850f46
AndreiMudrak <95141433+AndreiMudrak@users.noreply.github.com>
2022-05-26 11:50:39 +0300

    Update first.py

--- first.py
+++ first.py
@@ -1,9 +1,19 @@
+# My first Python code
+# После знака # можно оставлять комментарии или выключать (комментировать) часть кода
+
+# Загрузка библиотеки mcpi для коммуникации с сервером Minecraft
 import mcpi.minecraft as minecraft
 
+# Создаем объект, через который будем общаться с Майнкрафтом
+# Объект - это несколько переменных в одной
 mcraft = minecraft.Minecraft.create()
+# Получаем координаты игрока из объекта, созданного выше
 myCoord = mcraft.player.getTilePos()
 
+# Печатаем сообщения в чат в игре Майнкрафт
 mcraft.postToChat ("Hello world!")
+# Из объекта myCoord печатаем координаты игрока в чат
 mcraft.postToChat ("I'm here: x=" + str(myCoord.x) + " y=" + str(myCoord.y) + " z=" + str(myCoord.z))
 
-print ("My coordinate: x=" + str(myCoord.x) + " y=" + str(myCoord.y) + " z=" + str(myCoord.z))
+# Печатаем сообщения в терминале
+print ("My coordinate in Minecraft: x=" + str(myCoord.x) + " y=" + str(myCoord.y) + " z=" + str(myCoord.z))
